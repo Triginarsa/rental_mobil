77ad9b7e7652a9fd196dc5968a7a091e9fbc289c
evansaragih <evansaragih@student.unud.ac.id>
2018-04-06 19:32:42 +0800

    update 6 April malam

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 	public function index(){
-		$this->load->view('welcome_message');
-		echo base_url();
+		$this->load->view('beranda');
+		//echo base_url();
 		// $data1= $this->mymodel->get_provinsi_query();
 		// $this->load->view('tabel', array('data1' => $data1));
 	}
